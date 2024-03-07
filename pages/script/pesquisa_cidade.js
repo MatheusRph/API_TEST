65f693fa827905c1f58599b1b03a236b8cdc9734
Theus <144968520+MatheusRph@users.noreply.github.com>
2024-03-07 12:30:41 +0000

    Modifc na pesquisa input, req e prev

--- pages/script/pesquisa_cidade.js
+++ pages/script/pesquisa_cidade.js
@@ -1,10 +1,20 @@
-function pesquisa_cidade(event) {
+function pesquisa_cidade_req(event) {
     if (event.key === "Enter") {
         showLoading();
         const inputElement = document.getElementById("pesquisa");
         const city = inputElement.value;
         
         req(city);
+        prev(city);
+    }
+}
+
+function pesquisa_cidade_prev(event) {
+    if (event.key === "Enter") {
+        showLoading();
+        const inputElement = document.getElementById("pesquisa");
+        const city = inputElement.value;
+        
         prev(city);
     }
 }
