32be8f1bcc2412038f2544691b78e00f2eb4ea6d
MatheusRph <matheus.rodolpho16@gmail.com>
2024-04-10 16:10:18 -0300

    Arquivos Comentados

--- pages/script/prev.js
+++ pages/script/prev.js
@@ -1,67 +1,66 @@
-function prev(city){
+// Define a function called 'prev' that takes a 'city' parameter
+function prev(city) {
+
+    // Fetch weather data for the specified city from the '/prev/${city}' endpoint
     fetch(`/prev/${city}`)
-    .then(response => {
+     .then(response => {
+  
+        // Check if the response status is OK (2xx range)
         if (response.ok) {
-            return response.json();
+  
+          // Parse the response as JSON
+          return response.json();
         }
+  
+        // Throw an error with a custom error message if the response status is not OK
         throw new Error('Erro ao buscar dados climáticos: ' + response.status);
-    })
-    .then(data => {
+      })
+  
+      // Process the JSON response and extract the 'mediasTemp' and 'probabilidadeClima' arrays
+     .then(data => {
         const mediasTemp = data.mediasTemp;
-        // const probabilidadeClima = data.probabilidadeClima;
-
-        // // Agora você pode usar essas informações como desejar
-        // console.log('Médias de temperatura:', mediasTemp);
-        // console.log('Probabilidade de clima:', probabilidadeClima);
-        
-        for(let i = 0; i < 5; i++) {
-            // console.log(`Média dia${i+1}: ` + data.mediasTemp[i])
-            const tempDia = document.getElementById(`tempD${i}`);
-            // const temperature = data.mediasTemp[i];
-            // const clima = document.getElementById(`weatherPng${i}`);
-            
-            // for (let b = 0; i<=32; i+=8){
-            //     const climaDescri = data.probabilidadeClima[`${b}`]
-            //     console.log(climaDescri);
-                
-            //     if (climaDescri === "Tempo nublado") {
-            //         document.getElementById('climaPng').src = "img/clima/nublado.gif";
-            //     } else if (climaDescri === "Poucas Nuvens" || climaDescri === "Nuvens Quebradas" || climaDescri === "Nuvens Esparsas") {
-            //         document.getElementById('climaPng').src = "img/clima/nublado.gif";
-            //     } else if (climaDescri === "Chuva leve" || climaDescri === "Chuva moderada") {
-            //         document.getElementById('climaPng').src = "img/clima/chuva.gif";
-            //     } else if (climaDescri === "Nevoeiro" || climaDescri === "Nevoeiro") {
-            //         document.getElementById('climaPng').src = "img/clima/enevoado.gif";
-            //     } else if (climaDescri === "Céu Limpo") {
-            //         document.getElementById('climaPng').src = "img/clima/sol.gif";
-            //     } else if (climaDescri === "Chuvisco de Baixa Intensidade" || climaDescri === "Chuvisco" || climaDescri === "Chuvisco de Alta Intensidade" || climaDescri === "Chuva Fraca" || climaDescri === "Chuva" || climaDescri === "Chuva Forte" || climaDescri === "Chuva Muito Forte" || climaDescri === "Chuva Extrema" || climaDescri === "Chuva Congelante") {
-            //         document.getElementById('climaPng').src = "img/clima/chuva.gif";
-            //     } else if (climaDescri === "Neve Fraca" || climaDescri === "Neve" || climaDescri === "Neve Forte" || climaDescri === "Aguaneve" || climaDescri === "Aguaceiro com Neve" || climaDescri === "Chuva e Neve Fraca" || climaDescri === "Chuva e Neve" || climaDescri === "Neve Fraca" || climaDescri === "Neve" || climaDescri === "Neve Forte") {
-            //         document.getElementById('climaPng').src = "img/clima/neve.gif";
-            //     } else if (climaDescri === "Tempestade com Chuva Fraca" || climaDescri === "Tempestade com Chuva" || climaDescri === "Tempestade com Chuva Forte" || climaDescri === "Trovoada Fraca" || climaDescri === "Trovoada" || climaDescri === "Trovoada Forte" || climaDescri === "Trovoada Irregular" || climaDescri === "Trovoada com Chuvisco Fraco" || climaDescri === "Trovoada com Chuvisco" || climaDescri === "Trovoada com Chuvisco Forte") {
-            //         document.getElementById('climaPng').src = "img/clima/tempestade.gif";
-            //     } else {
-            //         // Se não houver correspondência, defina uma imagem padrão
-            //         document.getElementById('climaPng').src = "img/clima/sol.jpg";
-            //     }      
-
-            //     clima.src = climaDescri
-            // }
-
-            // if (i != 0) {
-            //     tempDia.innerHTML = `${temperature}<sub class="displayflex" style="font-size: 10px;">C</sub>`;
-            // }
-
-            if (tempDia) {
-                tempDia.innerHTML = `${temperature}<sub id="celcius">C</sub>
-                <img id="temperaturePng" class="displayflex center-start" src="img/temperature2.png" alt="">`;
+        const probabilidadeClima = data.probabilidadeClima;
+  
+        // Iterate through the 'mediasTemp' and 'probabilidadeClima' arrays and update the user interface accordingly
+        for (let i = 0; i < 5; i++) {
+          const temperature = mediasTemp[i];
+          const climaDescri = probabilidadeClima[i];
+  
+          // Update the temperature
+          const tempDia = document.getElementById(`tempD${i}`);
+          if (tempDia) {
+            tempDia.innerHTML = `${temperature}<sub class="displayflex" style="font-size: 10px;">C</sub>`;
+          }
+  
+          // Update the weather icon
+          const clima = document.getElementById(`clima${i}`);
+          if (clima) {
+            if (climaDescri === "Tempo nublado" || climaDescri === "Poucas Nuvens" || climaDescri === "Nuvens Quebradas" || climaDescri === "Nuvens Esparsas") {
+              clima.src = "img/clima/nublado.gif";
+            } else if (climaDescri === "Chuva leve" || climaDescri === "Chuva moderada" || climaDescri === "Chuvisco de Baixa Intensidade" || climaDescri === "Chuvisco" || climaDescri === "Chuvisco de Alta Intensidade" || climaDescri === "Chuva Fraca" || climaDescri === "Chuva" || climaDescri === "Chuva Forte" || climaDescri === "Chuva Muito Forte" || climaDescri === "Chuva Extrema" || climaDescri === "Chuva Congelante") {
+              clima.src = "img/clima/chuva.gif";
+            } else if (climaDescri === "Nevoeiro") {
+              clima.src = "img/clima/enevoado.gif";
+            } else if (climaDescri === "Céu Limpo") {
+              clima.src = "img/clima/sol.gif";
+            } else if (climaDescri === "Neve Fraca" || climaDescri === "Neve" || climaDescri === "Neve Forte" || climaDescri === "Aguaneve" || climaDescri === "Aguaceiro com Neve" || climaDescri === "Chuva e Neve Fraca" || climaDescri === "Chuva e Neve" || climaDescri === "Neve Fraca" || climaDescri === "Neve" || climaDescri === "Neve Forte") {
+              clima.src = "img/clima/neve.gif";
+            } else if (climaDescri === "Tempestade com Chuva Fraca" || climaDescri === "Tempestade com Chuva" || climaDescri === "Tempestade com Chuva Forte" || climaDescri === "Trovoada Fraca" || climaDescri === "Trovoada" || climaDescri === "Trovoada Forte" || climaDescri === "Trovoada Irregular" || climaDescri === "Trovoada com Chuvisco Fraco" || climaDescri === "Trovoada com Chuvisco" || climaDescri === "Trovoada com Chuvisco Forte") {
+              clima.src = "img/clima/tempestade.gif";
+            } else {
+              // Set a default weather icon if no match is found
+              clima.src= "img/clima/sol.jpg";
             }
-
+          }
         }
-
+  
+        // Hide the loading spinner
         hideLoading();
-    })
-    .catch(error => {
+      })
+  
+      // Handle any errors that might occur during the fetch request or data processing
+     .catch(error => {
+        // Hide the loading spinner on error
         hideLoading();
-    });
-}
+     });
+  }
--- pages/script/req.js
+++ pages/script/req.js
@@ -1,19 +1,21 @@
-function req (city){
-    fetch(city)
-    .then(response => {
+function req(cidade) { // função para fazer uma requisição fetch à API de clima
+    fetch(cidade)
+   .then(response => {
         if (response.ok) {
-            return response.json();
+            return response.json(); // se a resposta for ok, retorna o json
         }
-        throw new Error('Erro ao buscar dados climáticos: ' + response.status);
+        throw new Error('Erro ao buscar dados climáticos: ' + response.status); // se não, lança um erro com a mensagem e o status da resposta
     })
-    .then(data => {
+   .then(data => {
+        // preenche os elementos do DOM com os dados do clima
         document.getElementById('city').textContent = data.Nome;
         document.getElementById('C').innerHTML = 'C';
         document.getElementById('temperature').innerHTML = Math.round(data.Temperatura);
         document.getElementById('humidity').textContent = data.Umidade + '%';
         document.getElementById('windSpeed').textContent = data.VelocidadeDoVento + 'km/h';
         document.getElementById('weather').textContent = data.Clima;
-        
+
+        // define a imagem do clima de acordo com o clima atual
         if (data.Clima === "Tempo nublado") {
             document.getElementById('climaPng').src = "img/clima/nublado.gif";
         } else if (data.Clima === "Poucas Nuvens" || data.Clima === "Nuvens Quebradas" || data.Clima === "Nuvens Esparsas") {
@@ -31,17 +33,14 @@ function req (city){
         } else if (data.Clima === "Tempestade com Chuva Fraca" || data.Clima === "Tempestade com Chuva" || data.Clima === "Tempestade com Chuva Forte" || data.Clima === "Trovoada Fraca" || data.Clima === "Trovoada" || data.Clima === "Trovoada Forte" || data.Clima === "Trovoada Irregular" || data.Clima === "Trovoada com Chuvisco Fraco" || data.Clima === "Trovoada com Chuvisco" || data.Clima === "Trovoada com Chuvisco Forte") {
             document.getElementById('climaPng').src = "img/clima/tempestade.gif";
         } else {
-            // Se não houver correspondência, defina uma imagem padrão
+            // se não houver correspondência, defina uma imagem padrão
             document.getElementById('climaPng').src = "img/clima/sol.jpg";
-        }      
-        setTimeout(hideLoading, 2100);          
+        }
+        setTimeout(hideLoading, 2100); // esconde o loading após 2,1 segundos
     })
     .catch(error => {
-        hideLoading();
-        document.getElementById('temperature').textContent = 'Não foi possível obter os dados';
-        document.getElementById('humidity').textContent = 'Não foi possível obter os dados';
-        document.getElementById('windSpeed').textContent = 'Não foi possível obter os dados';
-        document.getElementById('weather').textContent = 'Não foi possível obter os dados';
-        console.error(error.message);
+        hideLoading(); // esconde o loading
+        // preenche os elementos do DOM com a mensagem de erro
+        document.getElementById('temperature').textContent = 'Erro ao obter dados';
     });
 }
